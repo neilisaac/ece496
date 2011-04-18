46a80342856e01ff887ee87521595d9d17ae9568
Neil Isaac <isaac.neil@gmail.com>
2011-04-17 23:15:18 -0400

    Generate placements directly in synthesize_cells.py and add CSV.

--- prototype/generate.csh
+++ prototype/generate.csh
@@ -2,7 +2,7 @@
 
 set rename = "output"
 set proj = "evolution"
-set top = "individual"
+set module = "individual"
 set family = "Cyclone II"
 set device = "EP2C20F484C7"
 
@@ -11,7 +11,7 @@ if ($#argv >= 1) then
 endif
 
 # delete old files
-rm -rf db incremental_db $proj.* $top.*
+rm -rf db incremental_db $proj.* $module.*
 
 set out = "$proj.qsf"
 set tcl = "set_global_assignment -name"
@@ -20,10 +20,10 @@ set q = '"'
 echo "creating $out"
 cat /dev/null > $out
 echo "$tcl PROJECT_CREATION_TIME_DATE $q`date '+%T  %B %d, %Y'`$q" >> $out
-echo "$tcl TOP_LEVEL_ENTITY $top" >> $out
+echo "$tcl TOP_LEVEL_ENTITY $module" >> $out
 echo "$tcl FAMILY $q$family$q" >> $out
 echo "$tcl DEVICE $q$device$q" >> $out
-echo "$tcl VERILOG_FILE $top.v" >> $out
+echo "$tcl VERILOG_FILE $module.v" >> $out
 echo "$tcl CYCLONEII_OPTIMIZATION_TECHNIQUE AREA" >> $out
 echo "$tcl SYNTHESIS_EFFORT FAST" >> $out
 echo "$tcl TRUE_WYSIWYG_FLOW ON" >> $out
@@ -33,8 +33,12 @@ echo "$tcl ADV_NETLIST_OPT_SYNTH_WYSIWYG_REMAP ON" >> $out
 set echo
 
 ./synthesize_cells.py \
-	--file $top.v \
-	--module $top \
+	--verilog $module.v \
+	--module $module \
+	--csv $module.csv \
+	--place $module.place \
+	--min-x 5 --max-x 12 \
+	--min-y 3 --labs 2,10,18,26 \
 	--input PIN_A13 \
 	--output PIN_B13 \
 	--cells 128 || exit 1
@@ -42,36 +46,39 @@ set echo
 # complete functional synthesis
 quartus_map $proj || exit 1
 
-# get synthesized instance names and fix them
-quartus_cdb $proj --vqm=$top.vqm
+## get synthesized instance names and fix them
+#quartus_cdb $proj --vqm=$module.vqm
+#
+#./fix_placement.py $module.vqm $proj.place \
+#	--instance-patterns "table" --cell cycloneii_lcell_comb \
+#	--min-x 19 --min-y 2 --max-x 26 \
+#	--fix-luts --luts-per-lab 3 || exit 1
 
-./fix_placement.py $top.vqm $proj.place \
-	--instance-patterns "table" --cell cycloneii_lcell_comb \
-	--min-x 19 --min-y 2 --max-x 26 \
-	--fix-luts --luts-per-lab 3 || exit 1
+# append placement information to the project file
+cat $module.place >> $proj.qsf
 
-cat $proj.place >> $proj.qsf
+## need to delete the synthesized verilog first
+#rm $module.vqm 
 
-# run placement and routing
-# need to delete the synthesized verilog first
-rm $top.vqm 
+# run placement and routing, then produce output files
 quartus_fit $proj || exit 1
-
 quartus_asm $proj || exit 1
 
+## program board
 #quartus_pgm -c USB_Blaster -m JTAG -o "P;$proj.sof" || exit 1
 
-quartus_cdb $proj --vqm=$top.vqm
-quartus_cdb $proj --back_annotate=lab
-quartus_cdb $proj --back_annotate=routing
+## back-annotate results for debugging
+#quartus_cdb $proj --vqm=$module.vqm
+#quartus_cdb $proj --back_annotate=lab
+#quartus_cdb $proj --back_annotate=routing
 
 unset echo
 
 echo saving result to $rename.tgz and $rename.sof
-
 mv $proj.sof $rename.sof
 
+# clean up
 rm -f $proj.pof
-#tar czf $rename.tgz $proj.* $top.*
+#tar czf $rename.tgz $proj.* $module.*
 #rm -rf db incremental_db
 
--- prototype/synthesize_cells.py
+++ prototype/synthesize_cells.py
@@ -6,18 +6,45 @@ import random
 
 parser = optparse.OptionParser()
 
-parser.add_option("-f", "--file", action="store", type="string",
-		dest="filename", default="output.v", help="output file name")
+parser.add_option("-v", "--verilog", action="store", type="string",
+		dest="filename", default="mutated_individual.v",
+		help="output verilog file name")
+
 parser.add_option("-m", "--module", action="store", type="string",
 		dest="module", default="mutated_individual", help="verilog module name")
+
 parser.add_option("-i", "--input", action="store", type="string",
 		dest="input", default="in", help="input signal")
+
 parser.add_option("-o", "--output", action="store", type="string",
-		dest="output", default="out", help="input signal")
+		dest="output", default="out", help="output signal")
+
+parser.add_option("-f", "--csv", action="store", type="string",
+		dest="csv", default=None, help="store configuration to specified CSV file")
+
 parser.add_option("-c", "--cells", action="store", type="int",
 		dest="cells", default=10, help="number of LEs")
+
+parser.add_option("-p", "--place", action="store", type="string",
+		dest="place", default=None,
+		help="enable fixed placement and set output file")
+
+parser.add_option("-l", "--labs", action="store", type="string",
+		dest="luts", default="0",
+		help="comma-separated list of LUT numbers to use in each LAB")
+
+parser.add_option("-x", "--min-x", action="store", type="int",
+		dest="minx", default=1, help="starting x LAB")
+		
+parser.add_option("-y", "--min-y", action="store", type="int",
+		dest="miny", default=1, help="starting y LAB")
+		
+parser.add_option("-X", "--max-x", action="store", type="int",
+		dest="maxx", default=16, help="maximum x LAB")
+
 parser.add_option("-s", "--seed", action="store", type="string",
-		dest="seed", default=None, help="seed vqm file")
+		dest="seed", default=None, help="seed CSV file")
+
 parser.add_option("-k", "--keep", action="store", type="int",
 		dest="keep", default=0, help="number of LE functions to keep")
 
@@ -39,73 +66,109 @@ if options.cells <= 0:
 	print "--cells must be at least 1"
 	sys.exit(1)
 
-out = open(options.filename, "w")
+verilog = open(options.filename, "w")
+
+# convert LUT string to integer list
+options.luts = [int(l) for l in options.luts.split(",")]
+options.luts.sort(reverse=True)
+
+place = False
+if options.place is not None:
+	place = open(options.place, "a+")
+
+csv = False
+if options.csv is not None:
+	csv = open(options.csv, "w")
 
 # create module
-print >>out, "module %s (" % options.module
-print >>out, "\t%s," % options.input
-print >>out, "\t%s\n);\n" % options.output
+print >>verilog, "module %s (" % options.module
+print >>verilog, "\t%s," % options.input
+print >>verilog, "\t%s\n);\n" % options.output
 
-print >>out, "input %s;" % options.input
-print >>out, "output %s;\n" % options.output
+print >>verilog, "input %s;" % options.input
+print >>verilog, "output %s;\n" % options.output
 
 # define names for all wires that can drive a value
 outputs = ["vdd", "gnd"] + ["table_%04d_out" % i for i in range(options.cells)]
 for wire in outputs:
-	print >>out, "(* keep *) wire %s;" % wire
+	print >>verilog, "(* keep *) wire %s;" % wire
 
 # the module's input can also drive cell inputs
 outputs.append(options.input)
 
 # set values for vdd and gnd
-print >>out, "\nassign vdd = 1'b1;"
-print >>out, "assign gnd = 1'b0;\n"
+print >>verilog, "\nassign vdd = 1'b1;"
+print >>verilog, "assign gnd = 1'b0;\n"
 
 # connect the output of cell 0 to the module's output
-print >>out, "assign %s = table_0000_out;\n" % options.output
+print >>verilog, "assign %s = table_0000_out;\n" % options.output
 
 # keep set of used outputs
 used = set()
 
 # create the lookup tables
+x = options.minx
+y = options.miny
 for i in range(options.cells):
 	# instantiate module
 	name = "table_%04d" % i
-	print >>out, "\ncycloneii_lcell_comb %s (" % name
+	print >>verilog, "\n\ncycloneii_lcell_comb %s (" % name
+
+	data = list()
 
 	# create input wires with randomly assigned drivers
 	for letter in ["a", "b", "c", "d"]:
 		wire = random.choice(outputs)
-		print >>out, "\t.data%s(%s)," % (letter, wire)
+		print >>verilog, "\t.data%s(%s)," % (letter, wire)
 		used.add(wire)
+		data.append(wire)
 
 	# assign module output
-	print >>out, "\t.combout(%s_out) );\n" % name;
+	print >>verilog, "\t.combout(%s_out) );\n" % name;
 
 	# set the module's function
 	mask = random.randint(0x0000, 0xFFFF)
-	print >>out, "defparam %s .lut_mask = \"%04X\";\n" % (name, mask)
+	print >>verilog, "defparam %s .lut_mask = \"%04X\";" % (name, mask)
 
-	# not sure what assigning .sum_lutc_input gives
-	#print >>out, "defparam %s .sum_lutc_input = \"cin\";" % name
+	## not sure what assigning .sum_lutc_input gives
+	#print >>verilog, "defparam %s .sum_lutc_input = \"cin\";" % name
 
-# end of module
-print >>out, "endmodule"
+	# fix placement
+	n = options.luts[i % len(options.luts)]
+	if place:
+		print >>verilog, "/* placment assigned to LCCOMB_X%d_Y%d_N%d */" % (x, y, n)
+		print >>place, "set_location_assignment LCCOMB_X%d_Y%d_N%d -to %s_out" % (x, y, n, name)
+		if i % len(options.luts) == len(options.luts) - 1:
+			x += 1
+			if x > options.maxx:
+				x = options.minx
+				y += 1
 
-out.close()
+	# save LUT to CSV
+	if csv:
+		print >>csv, "%s,%s,%s,%s,%s,%s_out,%04X,%d,%d,%d" % (name,
+				data[0], data[1], data[2], data[3], name, mask, x, y, n)
 
+# make sure the input signals are assigned
 if options.input not in used:
 	print "ERROR: input signal %s was not used!" % options.input
 	sys.exit(2)
-else:
-	used.remove(options.input)
 
-if "vdd" in used:
-	used.remove("vdd")
-if "gnd" in used:
-	used.remove("gnd")
+# check for unused output signals that would otherwise get synthesized out
+unused = set(outputs).difference(used)
+if len(unused) > 0:
+	print "WARNING: %d unused signals: %s" % (len(unused), " ".join(unused))
+
+# end of module
+print >>verilog, "\n\nendmodule\n"
+
+# close files
+verilog.close()
+if csv:
+	csv.close()
+if place:
+	place.close()
 
 print "created module: %s" % options.module
-print "used %d of %d output signals" % (len(used), len(outputs) - 3)
 print "saved to file: %s" % options.filename
 
