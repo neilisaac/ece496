420e12ef9d27dcc88feb3cd28a3793135727ed84
Neil Isaac <isaac.neil@gmail.com>
2011-05-05 23:41:30 -0400

    Randomly merge two CSV files line by line.

---
+++ prototype/merge_individuals.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python2
+
+import sys
+import random
+import optparse
+
+parser = optparse.OptionParser()
+
+parser.usage = "%prog [options] input1.csv input2.csv [output.csv]"
+
+options, args = parser.parse_args(sys.argv)
+
+if not (3 <= len(args) <= 4):
+	parser.print_help()
+	sys.exit(1)
+
+input1 = args[1]
+input2 = args[2]
+output = None
+if len(args) == 4:
+	output = args[3]
+
+out = sys.stdout
+if output is not None:
+	out = open(output, 'w')
+
+count = [0, 0]
+for lines in zip(open(input1).readlines(), open(input2).readlines()):
+	pick = random.randint(0, 1)
+	count[pick] += 1
+	print >> out, lines[pick],
+
+print >> sys.stderr, "picked %d lines from %s and %d from %s" % \
+		(count[0], input1, count[1], input2)
+
+if output is not None:
+	print >> sys.stderr, "saved result to " + output
+
