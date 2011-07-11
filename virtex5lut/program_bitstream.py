e521b22ff33da62176b48279774c1cbeb9fdf5aa
Neil Isaac <isaac.neil@gmail.com>
2011-07-11 00:40:01 -0400

    Deleted irrelevant stuff from program_bitstream.py.

--- virtex5lut/program_bitstream.py
+++ virtex5lut/program_bitstream.py
@@ -9,17 +9,11 @@ import optparse
 parser = optparse.OptionParser()
 
 parser.add_option("-d", "--device", dest="device", type="string",
-		default="/dev/ttyUSB0", help="serial port device")
+		default="/dev/ttyS0", help="serial port device")
 parser.add_option("-b", "--baud", dest="baud", type="int",
-		default=9600, help="device baud rate")
+		default=115200, help="device baud rate")
 parser.add_option("-t", "--timeout", dest="timeout", type="float",
 		default=0.1, help="device write timeout")
-parser.add_option("-s", "--samples", dest="samples", type="int",
-		default=10, help="number of smaples")
-parser.add_option("-f", "--failures", dest="failures", type="int",
-		default=4, help="maximum number of tolerated failures")
-parser.add_option("-o", "--output", dest="output", type="string",
-		default="individual.score", help="output file containing score")
 
 options, args = parser.parse_args(sys.argv)
 
@@ -28,50 +22,9 @@ s = serial.Serial(options.device,baudrate=options.baud, timeout=options.timeout,
 
 time.sleep(0.001)
 
-failed = 0
-scores = list()
+value = 0xFFFE # function: z = x3 & x2 & x1 & ~x0
 
-while len(scores) < options.samples:
-	# run test and request result
-	s.write("S")
-	data = s.read()
-
-	if len(data) != 1 or data != "R":
-		print "test failed: no acknowledgment"
-		failed += 1
-		if failed >= options.failures:
-			print "ERROR: maximum read failures reached"
-			sys.exit(1)
-		else:
-			continue
-
-	score = 0
-	data = s.read(4)
-	if len(data) != 4:
-		print "test failed: invalid score bytes"
-		failed += 1
-		if failed >= options.failures:
-			print "ERROR: maximum read failures reached"
-			sys.exit(1)
-		else:
-			continue
-
-	for value, shift in zip(data, [0, 8, 16, 24]):
-		score += ord(value) << shift
-	
-	print "read score: %8d (0x%08X)" % (score, score)
-	scores.append(score)
-
-mean = stats.lmean(scores)
-print "average score: %.2f" % mean
-
-out = open(options.output, "w")
-print >> out, mean
-
-if options.samples > 1 and mean != 0:
-	stdev = stats.lstdev(scores)
-	rating = mean / stdev
-
-	print "standard deviation: %.2f" % stdev
-	print "rating: %.6f" % rating
+s.write(chr(0x00))					# disable flop
+s.write(chr((value >> 8) & 0xFF))	# upper 8 bytes
+s.write(chr(value & 0xFF))			# lower 8 bytes
 
