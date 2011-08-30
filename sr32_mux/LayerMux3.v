e0fbbf55c4e0c52b8bf4ad62b35005b2fe63b813
Keyi Shi <keyi.shi@utoronto.ca>
2011-08-29 20:52:12 -0400

    Added 25 and 125 input muxes

---
+++ sr32_mux/LayerMux3.v
@@ -0,0 +1,68 @@
+module LayerMux3(input [124:0] A, input CE, input CLK, input D, output Z, output Q);
+
+parameter N = 125; //number of inputs desired, max = 125 (if under 26, use the 2-layer mux instead)
+parameter lay1 = N/5+(N%5 == 0 ? 0 : 1); //# of LUTs in the first layer
+parameter lay2 = lay1/5+(lay1%5 == 0 ? 0 : 1); //# of LUTs in the second layer
+
+//Making a 3-layer mux with 5 LUTs:
+//(N/5+(N%5 == 0 ? 0 : 1)) = # of LUTs in first layer
+//(N/5-(N%5 == 0 ? 1 : 0)) = # of LUTs in first layer -1
+//((N/5+(N%5 == 0 ? 0 : 1))/5+((N/5+(N%5 == 0 ? 0 : 1))%5 == 0 ? 0 : 1)) = # of LUTs in second layer
+//((N/5+(N%5 == 0 ? 0 : 1))/5-((N/5+(N%5 == 0 ? 0 : 1))%5 == 0 ? 1 : 0)) = # of LUTs in second layer -1
+
+wire [(lay2*5)-1:0] b; //output bus from layer 1 (sized according to number of inputs in layer 2)
+wire [lay2-1:0] c; //output bus from layer 2
+wire [lay1+lay2:0] scan_chain;
+
+assign scan_chain[0] = D;
+
+genvar i;
+
+//generate first layer: ceil(N/5) LUTs
+generate
+	 for (i=0; i<lay1; i=i+1) begin : SR321
+		SRLC32E #(
+		.INIT(32'hCCCCCCCC) // Initial Value of Shift Register
+		) L1A (
+		.Q(b[i]), // SRL data output
+		.Q31(scan_chain[i+1]), // SRL cascade output pin
+		.A(A[(i*5+4):(i*5)]), // 5-bit shift depth select input
+		.CE(CE), // Clock enable input
+		.CLK(CLK), // Clock input
+		.D(scan_chain[i]) // SRL data input
+		);
+		// End of SRLC32E_inst instantiation
+	 end
+endgenerate
+
+//generate second layer: ceil(ceil(N/5)/5) LUTs
+generate
+	 for (i=0; i<lay2; i=i+1) begin : SR322
+		SRLC32E #(
+		.INIT(32'hAAAAAAAA) // Initial Value of Shift Register
+		) L2A (
+		.Q(c[i]), // SRL data output
+		.Q31(scan_chain[lay1+i+1]), // SRL cascade output pin
+		.A(b[(i*5+4):(i*5)]), // 5-bit shift depth select input
+		.CE(CE), // Clock enable input
+		.CLK(CLK), // Clock input
+		.D(scan_chain[lay1+i]) // SRL data input
+		);
+		// End of SRLC32E_inst instantiation
+	 end
+endgenerate
+
+//final layer
+SRLC32E #(
+.INIT(32'hAAAAAAAA) // Initial Value of Shift Register
+) W (
+.Q(Z), // SRL data output
+.Q31(Q), // SRL cascade output pin
+.A(c), // 5-bit shift depth select input
+.CE(CE), // Clock enable input
+.CLK(CLK), // Clock input
+.D(scan_chain[lay1+lay2]) // SRL data input
+);
+// End of SRLC32E_inst instantiation
+
+endmodule
---
+++ sr32_mux/Mux2LayerTest.v
@@ -0,0 +1,132 @@
+module Mux2LayerTest(
+	input SYSCLK,
+	input SYSRST,
+	input PUSH_N, PUSH_S, PUSH_E, PUSH_W, PUSH_C,
+	input [7:0] DIP,
+	input UART_RX,
+	output UART_TX,
+	output LED_N, LED_S, LED_E, LED_W, LED_C,
+	output [7:0] LEDS
+   );
+	
+wire [24:0] a;
+assign a[7:0] = DIP[7:0];
+assign a[24:8] = 17'hFFFFF;
+
+reg user_clk;
+reg user_seen_edge;
+always @ (posedge SYSCLK)
+	if (~user_seen_edge && PUSH_N) begin
+		user_clk <= 1;
+		user_seen_edge <= 1;
+	end
+	else if (~PUSH_N) begin
+		user_seen_edge <= 0;
+		user_clk <= 0;
+	end
+	else begin
+		user_clk <= 0;
+	end
+
+reg [7:0] shift_select;
+wire shift_head1;
+wire shift_head2 = 0;
+wire shift_head = (shift_select == 0) ? shift_head2 : shift_head1;
+reg shift_enable;
+reg seen_edge;
+
+always @ (posedge SYSCLK) begin
+	if (~PUSH_S)
+		seen_edge <= 0;
+		
+	if (PUSH_S && ~seen_edge) begin
+		shift_select <= 0;
+		shift_enable <= 1;
+		seen_edge <= 1;
+	end
+	else if (shift_select == 192) begin
+		shift_select <= 0;
+		shift_enable <= 0;
+	end
+	else if (shift_enable)
+		shift_select <= shift_select + 1;
+end
+
+wire [7:0] shift_addr = shift_select - 1;
+
+wire shift_master = (PUSH_E? 0 : shift_head);
+
+LUT6 # (
+	// reversed because we start from address 0
+	// 5555_5555 = pick 0
+	// 3333_3333 = pick 1
+	// 0F0F_0F0F = pick 2
+	// 00FF_00FF = pick 3
+	// 0000_FFFF = pick 4
+	// 0000_0001 = AND gate
+	.INIT(64'h0000_0001_3333_3333)
+) static_shift_rom1 (
+	.O(shift_head_a),
+	.I0(shift_addr[0]),
+	.I1(shift_addr[1]),
+	.I2(shift_addr[2]),
+	.I3(shift_addr[3]),
+	.I4(shift_addr[4]),
+	.I5(shift_addr[5])
+);
+
+LUT6 # (
+	// reversed because we start from address 0
+	// 5555_5555 = pick 0
+	// 3333_3333 = pick 1
+	// 0F0F_0F0F = pick 2
+	// 00FF_00FF = pick 3
+	// 0000_FFFF = pick 4
+	// 0000_0001 = AND gate
+	.INIT(64'h0000_0001_0000_0001)
+) static_shift_rom2 (
+	.O(shift_head_b),
+	.I0(shift_addr[0]),
+	.I1(shift_addr[1]),
+	.I2(shift_addr[2]),
+	.I3(shift_addr[3]),
+	.I4(shift_addr[4]),
+	.I5(shift_addr[5])
+);
+
+LUT6 # (
+	// reversed because we start from address 0
+	// 5555_5555 = pick 0
+	// 3333_3333 = pick 1
+	// 0F0F_0F0F = pick 2
+	// 00FF_00FF = pick 3
+	// 0000_FFFF = pick 4
+	// 0000_0001 = AND gate
+	.INIT(64'h0000_0001_0F0F_0F0F)
+) static_shift_rom3 (
+	.O(shift_head_c),
+	.I0(shift_addr[0]),
+	.I1(shift_addr[1]),
+	.I2(shift_addr[2]),
+	.I3(shift_addr[3]),
+	.I4(shift_addr[4]),
+	.I5(shift_addr[5])
+);
+
+wire shift_head_d = (shift_addr[6]? shift_head_b: shift_head_a);
+assign shift_head1 = (shift_addr >= 128? shift_head_c: shift_head_d);
+
+LayerMux2 mux_inst(
+.A(a),
+.CE(shift_enable),
+.CLK(SYSCLK),
+.D(shift_master),
+.Z(LEDS[7]),
+.Q(LED_C)
+);
+
+assign {LED_N, LED_E, LED_W, LED_S} = 4'b0;
+assign LEDS[6:0] = 7'b0;
+assign UART_TX = 1'b0;
+
+endmodule
