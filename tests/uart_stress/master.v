7bb7a2580752a818fa7afe0930802fcfd839ae2c
Neil Isaac <isaac.neil@gmail.com>
2011-09-12 15:23:00 -0400

    Added uart_stress to tests with improved uart.v.

---
+++ tests/uart_stress/master.v
@@ -0,0 +1,58 @@
+module MASTER (
+	input SYSCLK,
+	input SYSRST,
+	input PUSH_N, PUSH_S, PUSH_E, PUSH_W, PUSH_C,
+	input [7:0] DIP,
+	input UART_RX,
+	output UART_TX,
+	output LED_N, LED_S, LED_E, LED_W, LED_C,
+	output [7:0] LEDS
+);
+
+
+wire uart_rx_valid;
+wire uart_tx_ready;
+reg uart_tx_valid;
+wire [7:0] uart_rx_data;
+
+UART UART_inst (
+	.SCLK(SYSCLK),
+	.RESET(~SYSRST),
+	.RX(UART_RX),
+	.RX_VALID(uart_rx_valid),
+	.RX_DATA(uart_rx_data),
+	.TX(UART_TX),
+	.TX_READY(uart_tx_ready),
+	.TX_VALID(uart_tx_valid),
+	.TX_DATA(uart_rx_data)
+);
+
+reg [7:0] good, total;
+
+always @ (posedge SYSCLK) begin
+	uart_tx_valid <= 0;
+
+	if (PUSH_C) begin
+		good <= 0;
+		total <= 0;
+	end
+	else begin
+		if (uart_rx_valid) begin
+			total <= total + 1;
+
+			if (uart_rx_data == total)
+				good <= good + 1;
+
+			if (uart_tx_ready)
+				uart_tx_valid <= 1;
+		end
+	end
+end
+
+
+assign LEDS = PUSH_S ? total : good;
+assign { LED_N, LED_W, LED_S, LED_E, LED_C } = { PUSH_N, PUSH_W, PUSH_S, PUSH_E, PUSH_C };
+
+
+endmodule
+
---
+++ tests/uart_stress/stresstest.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python2 
+
+import sys
+import time
+import serial
+
+s = serial.Serial("/dev/ttyUSB0", baudrate=115200, timeout=1,
+		bytesize=8, parity=serial.PARITY_NONE)
+
+count = 0
+offset = 0
+for i in range(256):
+	s.write(chr(i))
+	time.sleep(0.01)
+	v = s.read()
+	if len(v) == 0:
+		break
+	if ord(v) == i + offset:
+		count += 1
+	else:
+		print "{:08b} -> {:08b}".format(i, ord(v))
+		#print "off by", ord(v) - i
+		#offset = ord(v) - i
+
+print count
+
