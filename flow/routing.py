a2981a10a12377162f7af72571805da833e68829
Neil Isaac <neil@neilisaac.ca>
2011-12-30 20:34:05 -0800

    Infer connection block and switch block sources and sinks.

--- flow/routing.py
+++ flow/routing.py
@@ -11,8 +11,11 @@ class Node:
 		self.qualifier = qualifier
 		self.value = value
 
+	def __repr__(self):
+		return "{:s} {:d} {:d} {:s} {:d}".format(self.kind, self.x, self.y, self.qualifier, self.value)
+
 	def dump(self):
-		print "node:", self.kind, self.x, self.y, self.qualifier, self.value
+		print repr(self)
 
 
 class Net:
@@ -61,12 +64,84 @@ class Routing:
 				qualifier = re.sub(":.*", "", line[2])
 				self.nets[-1].nodes.append(Node(line[0], x, y, qualifier, int(line[3])))
 	
+	def infer(self):
+		cby = [[list() for row in range(self.height + 1)] for col in range(self.width + 1)]
+		cbx = [[list() for row in range(self.height + 1)] for col in range(self.width + 1)]
+		sb = [[list() for row in range(self.height + 1)] for col in range(self.width + 1)]
+
+		for net in self.nets:
+			prev = None
+			for node in net.nodes:
+				if prev is None:
+					prev = node
+					continue
+
+				# connection block interconnect drivers
+				if node.kind == "CHANX":
+					cbx[node.x][node.y].append((node, prev))
+				elif node.kind == "CHANY":
+					cby[node.x][node.y].append((node, prev))
+
+				# connection block driving logic block or pad input
+				if node.kind == "IPIN":
+					if prev.kind == "CHANX":
+						cbx[prev.x][prev.y].append((node, prev))
+					elif prev.kind == "CHANY":
+						cby[prev.x][prev.y].append((node, prev))
+					else:
+						raise Exception, "unknown routing connection"
+
+				# switch block drives cb-cb routes
+				if prev.kind in ["CHANX", "CHANY"] and node.kind in ["CHANX", "CHANY"]:
+					x = min(prev.x, node.x)
+					y = min(prev.y, node.y)
+					sb[x][y].append((node, prev))
+
+				prev = node
+
+		return cby, cbx, sb
+	
 	def dump(self):
 		for net in self.nets:
 			print "net", net.num, net.name
 			for node in net.nodes:
 				print "\t", 
 				node.dump()
+
+		cby, cbx, sb = self.infer()
+
+		print "vertical connection blocks:"
+		x = 0
+		for col in cby:
+			y = 0
+			for row in col:
+				print "\tconnection block", x, y
+				for dst, src in row:
+					print "\t\t", src, "->", dst
+				y += 1
+			x += 1
+
+		print "horizontal connection blocks:"
+		x = 0
+		for col in cbx:
+			y = 0
+			for row in col:
+				print "\tconnection block", x, y
+				for dst, src in row:
+					print "\t\t", src, "->", dst
+				y += 1
+			x += 1
+
+		print "switch blocks:"
+		x = 0
+		for col in sb:
+			y = 0
+			for row in col:
+				print "\tswitch block", x, y
+				for dst, src in row:
+					print "\t\t", src, "->", dst
+				y += 1
+			x += 1
 
 
 if __name__ == '__main__':
