ecd8f671019396ac520fe953c7655d3e17eb966f
Neil Isaac <neil@neilisaac.ca>
2012-01-08 16:46:08 -0500

    Check exit codes in go.sh.

--- flow/go.sh
+++ flow/go.sh
@@ -2,11 +2,11 @@
 
 ./odin_ii -V test.v -o test.odin.blif
 
-./abc-vtr -f abc.cmd
+./abc-vtr -f abc.cmd || exit 1
 
 cat test.abc.blif | awk '{ if ($1 == ".latch"){ print $1, $2, $3, "re", "top^clk", $4; } else { print $0; } }' > test.awk.blif
 
-./t-vpack test.awk.blif test.net -inputs_per_cluster 16 -cluster_size 4 -lut_size 6
+./t-vpack test.awk.blif test.net -inputs_per_cluster 16 -cluster_size 4 -lut_size 6 || exit 1
 
-./vpr test.net k6-n4.xml place.out route.out  -route_chan_width 4
+./vpr test.net k6-n4.xml place.out route.out  -route_chan_width 4 || exit 1
 
