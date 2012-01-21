7bba3baacfc2f9dc534ff9fc093d05b16b316be6
Neil Isaac <neil@neilisaac.ca>
2012-01-21 16:00:33 -0500

    Run VPR with -nodisp.

--- flow/go.sh
+++ flow/go.sh
@@ -8,7 +8,7 @@ cat test.abc.blif | awk '{ if ($1 == ".latch"){ print $1, $2, $3, "re", "top^clk
 
 ./t-vpack test.awk.blif test.net -inputs_per_cluster 16 -cluster_size 4 -lut_size 6 || exit 1
 
-./vpr test.net k6-n4.xml place.out route.out  -route_chan_width 4 || exit 1
+./vpr test.net -nodisp k6-n4.xml place.out route.out  -route_chan_width 4 || exit 1
 
 ./fpga.py place.out route.out test.net test.abc.blif > test.bit || exit 1
 
