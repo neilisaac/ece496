e69acf58ff42fc647417fa517848423e24e1f9d5
Neil Isaac <neil@neilisaac.ca>
2012-04-02 18:25:14 -0400

    Changed demo.

--- flow/demo/demo.v
+++ flow/demo/demo.v
@@ -11,9 +11,14 @@ output [7:0] out;
 reg [7:0] buffer;
 
 always @ (posedge clk) begin
-	buffer <= buffer + 1;
+	if (buffer == 0)
+		buffer <= in;
+	else begin
+		buffer[7:1] <= buffer[6:0];
+		buffer[0] <= buffer[7];
+	end
 end
 
-assign out = buffer & in;
+assign out = buffer;
 
 endmodule
